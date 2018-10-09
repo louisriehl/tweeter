a9bcf253c552041163fcf7eaaf6b2a159e31eec9
Louis Riehl <louisriehl@gmail.com>
2018-10-09 16:09:25 +0000

    being over the char count disables submit

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,16 +2,25 @@
 $(document).ready(function () {
 console.log("Char counter loaded!");
 
-$(".new-tweet textarea").on("input", function () {
-  let count = 140 - $(this).val().length;
-  const counter = $(this).parent().children(".counter");
-  if (count < 0) {
-    $(counter).css("color", "red");
-  } else {
-    $(counter).css("color", "#c1c1c1");
-  }
-  $(counter).text(count);
+  $(".new-tweet textarea").on("input", function () {
+    let count = 140 - $(this).val().length;
+    const counter = $(this).parent().children(".counter");
+    const button = $(this).parent().children("input[type=submit]");
 
-});
+    if (count < 0) {
+      $(counter).css("color", "red");
+      $(button).attr("disabled", "disabled");
+    } else if (count >= 0) {
+      $(counter).css("color", "#c1c1c1");
+      $(button).removeAttr("disabled", "disabled");
+    }
+
+    $(counter).text(count);
+
+  });
+
+  $(".new-tweet").on("submit", function () {
+    console.log("submitted!");
+  });
 
 });
