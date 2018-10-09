3928c3b8a57221f2c42b860a89cca3f61668d592
Louis Riehl <louisriehl@gmail.com>
2018-10-09 16:02:40 +0000

    integrated tweet length checker

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,1 +1,17 @@
+
+$(document).ready(function () {
 console.log("Char counter loaded!");
+
+$(".new-tweet textarea").on("input", function () {
+  let count = 140 - $(this).val().length;
+  const counter = $(this).parent().children(".counter");
+  if (count < 0) {
+    $(counter).css("color", "red");
+  } else {
+    $(counter).css("color", "#c1c1c1");
+  }
+  $(counter).text(count);
+
+});
+
+});
