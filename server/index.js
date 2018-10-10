031cde03de11e6790fbf78f2aedb439c4c4f4e31
Louis Riehl <louisriehl@gmail.com>
2018-10-10 20:17:27 +0000

    refactored index, implemented get from mongodb

--- server/index.js
+++ server/index.js
@@ -13,37 +13,29 @@ app.use(express.static("public"));
 // Importing database from mongodb
 const MongoClient = require("mongodb").MongoClient;
 const MONGODB_URI = "mongodb://localhost:27017/tweeter";
-const database = { tweets: []};
+// const database = { tweets: []};
 
 MongoClient.connect( MONGODB_URI, (err, db) => {
   // Lazy, fix later!
   if(err) throw err;
 
-  db.collection("tweets").find().toArray( (err, tweets) => {
-    if (err) throw err;
-    tweets.forEach( tweet => {
-     database.tweets.push(tweet);
-    });
+const DataHelpers = require("./lib/data-helpers.js")(db);
 
-  db.close();
-  });
-});
 
-// The `data-helpers` module provides an interface to the database of tweets.
-// This simple interface layer has a big benefit: we could switch out the
-// actual database it uses and see little to no changes elsewhere in the code
-// (hint hint).
-//
-// Because it exports a function that expects the `db` as a parameter, we can
-// require it and pass the `db` parameter immediately:
-const DataHelpers = require("./lib/data-helpers.js")(database);
-
-// The `tweets-routes` module works similarly: we pass it the `DataHelpers` object
-// so it can define routes that use it to interact with the data layer.
 const tweetsRoutes = require("./routes/tweets")(DataHelpers);
 
-// Mount the tweets routes at the "/tweets" path prefix:
+
 app.use("/tweets", tweetsRoutes);
+
+  // db.collection("tweets").find().toArray( (err, tweets) => {
+  //   if (err) throw err;
+  //   tweets.forEach( tweet => {
+  //    database.tweets.push(tweet);
+  //   });
+
+
+});
+
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
