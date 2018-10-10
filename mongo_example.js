6f86da2f98a67ac0cb6f3809d6637ad5921d8f76
Louis Riehl <louisriehl@gmail.com>
2018-10-10 18:48:03 +0000

    example can retrieve tweets from server

--- mongo_example.js
+++ mongo_example.js
@@ -12,8 +12,30 @@ MongoClient.connect(MONGODB_URI, (err, db) => {
   // -- At this stage we have a connection to test-tweets db
   console.log(`Connected to: ${MONGODB_URI}`);
 
-  // -- Any logic that needs to utilize the connection is placed here.
-  // -- Then, we close the connection.
+  // ==> Refactored and wrapped as new, tweet-specific function:
 
-  db.close();
+  function getTweets(callback) {
+    db.collection("tweets").find().toArray((err, tweets) => {
+      if (err) {
+        return callback(err);
+      }
+      callback(null, tweets);
+    });
+  }
+
+  // ==> Later it can be invoked. Remember even if you pass
+  //     `getTweets` to another scope, it still has closure over
+  //     `db`, so it will still work. Yay!
+
+  getTweets((err, tweets) => {
+    if (err) throw err;
+
+    console.log("Logging each tweet:");
+    for (let tweet of tweets) {
+      console.log(tweet);
+    }
+
+    db.close();
+  });
+
 });
