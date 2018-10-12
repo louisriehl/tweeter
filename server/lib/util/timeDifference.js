5d5cf41fd432a3315033891a7ba06e6d33202509
Louis Riehl <louisriehl@gmail.com>
2018-10-12 18:52:46 +0000

    fixed moment depreciation

--- server/lib/util/timeDifference.js
+++ server/lib/util/timeDifference.js
@@ -2,6 +2,6 @@ const moment = require("moment");
 
 // Returns a date string in the format of `X time ago`
 module.exports = function timeDifference(date) {
-  let parsedTime = moment(date);
+  const parsedTime = moment(date);
   return parsedTime.fromNow();
 };
